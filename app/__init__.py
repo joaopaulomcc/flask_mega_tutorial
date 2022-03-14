d18816b3cc9c21f07ffad70c12b3952a56a83ae7
joaopaulomcc <joaopaulomcc@gmail.com>
2022-03-14 19:55:08 -0300

    Chapter 12 - Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 
@@ -19,6 +20,7 @@ login = LoginManager(application)
 login.login_view = "login"
 mail = Mail(application)
 bootstrap = Bootstrap(application)
+moment = Moment(application)
 
 
 from app import routes, models, errors
