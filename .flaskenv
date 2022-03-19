a82eacded22f86b2a59d1150ce5a5f5986a5aa9b
joaopaulomcc <joaopaulomcc@gmail.com>
2022-03-19 14:55:15 -0300

    Chapter 15 - A Better Application Structure

--- .flaskenv
+++ .flaskenv
@@ -1,10 +1,2 @@
 FLASK_APP=microblog.py
 FLASK_ENV=development
-MAIL_SERVER = "server"
-MAIL_PORT = 000
-MAIL_USE_TLS = True
-MAIL_USERNAME = "apikey"
-MAIL_PASSWORD = "password"
-MAIL_DEFAULT_SENDER = "example@example.com"
-MAIL_API_KEY = "apikey"
-ADMINS = ["example@example.com"]
--- config.py
+++ config.py
@@ -1,27 +1,21 @@
 import os
-
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you_will_never_guess"
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(
-        basedir, "app.db"
-    )
-
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    MAIL_SERVER = os.environ.get("MAIL_SERVER")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT"))
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSOWRD")
-    MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER")
-    MAIL_API_KEY = os.environ.get("MAIL_API_KEY")
-    ADMINS = os.environ.get("ADMINS")
-    POSTS_PER_PAGE = 3
-    LANGUAGES = ["en", "es"]
-    MS_TRANSLATOR_KEY = os.environ.get("MS_TRANSLATOR_KEY")
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    POSTS_PER_PAGE = 25
