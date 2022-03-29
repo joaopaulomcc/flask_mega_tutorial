1e226927a29034efac1cfebecabfc3d9721c6de5
joaopaulomcc <joaopaulomcc@gmail.com>
2022-03-29 20:05:00 -0300

    Chapter 21 - User Notifications

--- app/models.py
+++ app/models.py
@@ -5,6 +5,10 @@ from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+import json
+
+from time import time
+
 from app import db, login
 
 
@@ -31,9 +35,17 @@ class User(UserMixin, db.Model):
         backref=db.backref("followers", lazy="dynamic"),
         lazy="dynamic",
     )
+    messages_sent = db.relationship(
+        "Message", foreign_keys="Message.sender_id", backref="author", lazy="dynamic"
+    )
+    messages_received = db.relationship(
+        "Message", foreign_keys="Message.recipient_id", backref="recipient", lazy="dynamic"
+    )
+    last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship("Notification", backref="user", lazy="dynamic")
 
     def __repr__(self):
-        return "<User {}>".format(self.username)
+        return f"<User {self.username}>"
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -43,7 +55,7 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode("utf-8")).hexdigest()
-        return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(digest, size)
+        return f"https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}"
 
     def follow(self, user):
         if not self.is_following(user):
@@ -80,6 +92,23 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+    def new_messages(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+
+        return (
+            Message.query.filter_by(recipient=self)
+            .filter(Message.timestamp > last_read_time)
+            .count()
+        )
+
+    def add_notification(self, name, data):
+
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -94,4 +123,28 @@ class Post(db.Model):
     language = db.Column(db.String(5))
 
     def __repr__(self):
-        return "<Post {}>".format(self.body)
+        return f"<Post {self.body}>"
+
+
+class Message(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    recipient_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Message {self.body}>"
+
+
+class Notification(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
